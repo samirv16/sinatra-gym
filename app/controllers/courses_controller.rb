24e605a32544205a9eada4e6da75b1e82177dd29
samirv16 <samir.vallecillo@outlook.com>
2019-11-04 20:51:09 -0600

    creates a delete button and apporpriate views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,5 +6,42 @@ class CoursesController < ApplicationController
         erb :'courses/index'
     end
 
+    get '/courses/new' do
+        authenticate
+        erb :'courses/new'
+    end
+
+    post '/courses' do
+        authenticate
+        u = current_user
+        u.courses.build(name: params[:name], description: params[:description], price: params[:price])
+        if u.save
+            redirect '/courses'
+        else
+            @messege = "There was a problem creating your course!"
+            erb :'courses/new'
+        end
+    end
+
+    delete '/courses/:id' do 
+        authenticate
+        course = Course.find_by_id(params[:id])
+        if course
+            course.delete
+            redirect '/courses'
+        end
+    end
+
+    get '/courses/:id/edit' do
+        authenticate
+        @course = Course.find_by(id: params[:id])
+        if @course 
+            erb :'courses/edit'
+        else
+            erb :error, layout: false
+        end
+    end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
     validates :name, presence: true
     validates :username, presence: true, uniqueness: true
-    validates :password, length: {in: 8..16}
+    validates :password, length: {in: 8..16}, unless: -> (u){u.password.blank?}
 
 
     has_many :courses
